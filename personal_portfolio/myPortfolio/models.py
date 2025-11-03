ddd7c67db4bae23b5609975520ed1bf713da0703
Dipesh <dipeshluitel40@gmail.com>
2025-11-03 21:39:29 +0545

    drop down model is created

--- personal_portfolio/myPortfolio/models.py
+++ personal_portfolio/myPortfolio/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
 class Guest(models.Model):
     firstname = models.CharField(max_length=15,blank=False, verbose_name='enter your name')
     lastname = models.CharField(max_length=15,blank=False)
     email = models.EmailField(max_length=256,blank=False)
     content = models.TextField(verbose_name='Additional Message')
-    # dropdown = models.drop
+    dropdown = models.ForeignKey(Category, on_delete=models.CASCADE)
