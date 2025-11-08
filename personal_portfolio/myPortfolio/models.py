b32ed77d7945402e695716fd18e2a62b0d0366df
Dipesh <dipeshluitel40@gmail.com>
2025-11-08 19:08:36 +0545

    design projects page and do bug fixes with footer

--- personal_portfolio/myPortfolio/models.py
+++ personal_portfolio/myPortfolio/models.py
@@ -12,5 +12,5 @@ class Guest(models.Model):
     firstname = models.CharField(max_length=15,blank=False, verbose_name='enter your name')
     lastname = models.CharField(max_length=15,blank=False)
     email = models.EmailField(max_length=256,blank=False)
-    content = models.TextField(verbose_name='Additional Message')
-    dropdown = models.ForeignKey(Category, on_delete=models.CASCADE,default="")
+    content = models.TextField(verbose_name='Additional Message',blank=True)
+    dropdown = models.ForeignKey(Category, on_delete=models.CASCADE,)
